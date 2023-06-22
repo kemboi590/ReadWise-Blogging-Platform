4288db9d6ff40fe06898f8f02ae1ad604d58a308
kemboi590 <bkemboi590@gmail.com>
2023-06-22 10:12:44 +0300

    added loginRequired to all the routes

--- ServerSide/src/routes/CommentsRoute.js
+++ ServerSide/src/routes/CommentsRoute.js
@@ -5,15 +5,16 @@ import {
   deleteComment,
 } from "../controllers/commentsController.js";
 
+import { loginRequired } from "../controllers/userController.js";
 const comments = (app) => {
   //comment routes
 
-  app.route("/comments").post(createComment); //create comment to a post
+  app.route("/comments").post(loginRequired, createComment); //create comment to a post
 
-  app.route("/comments/:id").get(getComments); //get comments of a post
+  app.route("/comments/:id").get(loginRequired, getComments); //get comments of a post
 
-  app.route("/comments/:id").put(updateComment); //update comment
+  app.route("/comments/:id").put(loginRequired, updateComment); //update comment
 
-  app.route("/comments/:id").delete(deleteComment); //delete comment
+  app.route("/comments/:id").delete(loginRequired, deleteComment); //delete comment
 };
 export default comments;
--- ServerSide/src/routes/postRoute.js
+++ ServerSide/src/routes/postRoute.js
@@ -5,11 +5,12 @@ import {
   updateBlog,
   deleteBlog,
 } from "../controllers/postController.js";
+import { loginRequired } from "../controllers/userController.js";
 
 const blogs = (app) => {
   //blog routes
-  app.route("/blogs").get(getBlogs).post(createBlog);
-  app.route("/blogs/:id").get(getSingleBlog).put(updateBlog).delete(deleteBlog);
+  app.route("/blogs").get(loginRequired, getBlogs).post(loginRequired,createBlog);
+  app.route("/blogs/:id").get(loginRequired, getSingleBlog).put(loginRequired,updateBlog).delete(loginRequired, deleteBlog);
 };
 
 export default blogs;
