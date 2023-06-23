e29f3379de1dedc9f20376fcc101045fe1053d6b
kemboi590 <bkemboi590@gmail.com>
2023-06-23 15:51:54 +0300

    deleted blogData and added alert log out

--- ClientSide/src/pages/Logout/Logout.jsx
+++ ClientSide/src/pages/Logout/Logout.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import Home from "../Home/Home";
+import { useEffect } from "react";
 
 function Logout() {
+  useEffect(() => {
+    // Display an alert when the component mounts
+    alert("You have been logged out!");
+  }, []);
   return (
     <div>
       <Home />
