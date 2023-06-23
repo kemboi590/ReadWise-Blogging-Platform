16cd5e883205e91b79bdb710fc2298333f340319
kemboi590 <bkemboi590@gmail.com>
2023-06-23 19:57:40 +0300

    renamed my files

--- ClientSide/src/App.jsx
+++ ClientSide/src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
-// import pages and components
+// import  components
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+// import pages
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Blogs from "./pages/Blogs/Blogs";
@@ -11,12 +12,12 @@ import Contacts from "./pages/Contact/Contacts";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 import NotFound from "./pages/NotFound/NotFound";
-import TinyMCEEditor from "./pages/Blogs/TinyMCEEditor";
+import QuillEditor from "./pages/Blogs/QuillEditor";
 import BlogPosts from "./pages/Blogs/BlogPosts";
 import Profile from "./pages/profile/Profile";
 import Logout from "./pages/Logout/Logout";
-//6ysspo305d6uqauwzwnfy7ormdyq073g8jpw0gohu2a3m2gp
 
+// import context
 import { useContext } from "react";
 import { Context } from "./context/userContext/Context";
 
@@ -37,7 +38,7 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="*" element={<NotFound />} />
-          <Route path="/createblog" element={<TinyMCEEditor />} />
+          <Route path="/createblog" element={<QuillEditor />} />
           <Route path="/blogposts/:id" element={<BlogPosts />} />
           <Route path="/profile" element={user ? <Profile /> : <Login />} />
           <Route path="/logout" element={<Logout />} />
